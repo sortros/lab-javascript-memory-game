a829c509add30f734893e59664e4015e22884800
Josep <albiol957@hotmail.com>
2021-07-04 21:43:10 +0200

    it 2 in wip

--- js/memory.js
+++ js/memory.js
@@ -2,14 +2,38 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = pickedCards;
+    this.pairsClicked = pairsClicked;
+    this.pairsGuessed = pairsGuessed;
   }
 
   shuffleCards() {
     // ... write your code here
+    let m = array.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    if (card1.name === card2.name) {
+      pairsClicked++;
+      pairsGuessed++;
+    } else {
+      pairsClicked++;
+    }
   }
 
   checkIfFinished() {
